66ede38570bef4a4cf6f40d1feb782ed87cdaa99
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-12 19:22:23 -0300

    Adding State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-
+import React, { Fragment, useState } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
+
+  //state del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
   return (
-    <h1> 
-    Clima React
-    </h1>
+    <Fragment> 
+    <Header 
+      titulo='Clima React App'
+    />
+
+    <div className="contenedor-form">
+      <div className="container">
+        <div className="row">
+          <div className="col m6 s12">
+            <Formulario 
+              busqueda={busqueda}
+              guardarBusqueda={guardarBusqueda}
+            />
+          </div>
+          <div className="col m6 s12">
+            2
+          </div>
+        </div>
+      </div>
+    </div>
+    </Fragment>
   );
 }
 
